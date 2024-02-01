968272bb2e258e866e9f901bbef7e3c956b49ce2
Knugen <111962656+Dungeater1337@users.noreply.github.com>
2024-02-01 10:49:40 +0100

    dsdsdsdsd
    sdsdsdsdsdsdsdsds

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System; 
+class Program
+{
+    static void Main(string[] args)
+    {
+        while(true)
+        {
+            int result;
+            if(!TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Skriv en siffra!");
+            }
+            else
+            {
+                Console.WriteLine(result); 
+            }
+
+            static bool TryParse(string s, out int result)
+            {
+
+                try
+                {
+                    result = int.Parse(s);
+                    return true; 
+                }
+
+                catch
+                {
+                    result = -1;
+                    return false;                  
+                }
+            }
+        }
+    }
+}
